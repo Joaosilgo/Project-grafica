91062894dac3d9f4925559205eb4cf48510f8346
Joaosilgo <joaosilgo96@gmail.com>
2020-03-04 02:28:36 +0000

    Add files via upload
    v 3.0

--- src/js/site.js
+++ src/js/site.js
@@ -84,3 +84,15 @@ $(document).ready(function () {
     });
 
   });
+
+
+
+  
+// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
+
