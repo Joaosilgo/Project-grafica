ae471ff7600269d8eef94eaabcdbb2766b921482
João Gomes <joaosilgo96@gmail.com>
2021-04-03 23:39:40 +0100

    🍧

--- service-worker.js
+++ service-worker.js
@@ -137,3 +137,57 @@ this.addEventListener('fetch', event => {
 
 
 
+
+
+/**** START notificationclose ****/
+self.addEventListener('notificationclose', function (e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+
+/**** END notificationclose ****/
+
+
+
+
+/**** START notificationActionClickEvent ****/
+self.addEventListener('notificationclick', function (event) {
+  if (!event.action) {
+    // Was a normal notification click
+
+    console.log('Notification Click.');
+    clients.openWindow('https://orcamentosgraficos.pt/').then(function (client) {
+      client.navigate('https://orcamentosgraficos.pt/');
+    });
+    return;
+  }
+
+  switch (event.action) {
+    case 'coffee-action':
+      console.log('User ❤️️\'s coffee.');
+      // self.open('https://orcamentosgraficos.pt/', '_blank');
+      clients.openWindow('https://orcamentosgraficos.pt/');
+
+      break;
+    case 'doughnut-action':
+      console.log('User ❤️️\'s doughnuts.');
+      clients.openWindow('https://orcamentosgraficos.pt/').then(function (client) {
+        client.navigate('https://orcamentosgraficos.pt/');
+      });
+      break;
+    case 'gramophone-action':
+      console.log('User ❤️️\'s music.');
+      break;
+    case 'atom-action':
+      console.log('User ❤️️\'s science.');
+      break;
+    default:
+      console.log(`Unknown action clicked: '${event.action}'`);
+      break;
+  }
+});
+/**** END notificationActionClickEvent ****/
+
+
