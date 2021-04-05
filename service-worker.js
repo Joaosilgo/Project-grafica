c979d0bb560c36757677a1150f0efdb6f3760c41
João Gomes <joaosilgo96@gmail.com>
2021-04-05 03:21:52 +0100

    🍧

--- service-worker.js
+++ service-worker.js
@@ -15,11 +15,15 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 // Set a name for the current cache
+
+/* this is test
 var cacheName = 'v1';
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const offlineUrl = './offline.html';
+*/
 
+/* this is test
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './', // Alias for index.html
@@ -28,7 +32,10 @@ const PRECACHE_URLS = [
   './manifest.json',
   offlineUrl
 ];
+*/
 
+
+/* this is test
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   console.log('[ServiceWorker] Installed');
@@ -38,6 +45,8 @@ self.addEventListener('install', event => {
       .then(self.skipWaiting())
   );
 });
+
+*/
 
 /*
 // The activate handler takes care of cleaning up old caches.
@@ -70,6 +79,7 @@ self.addEventListener('activate', (event) => {
 */
 
 
+/* this is test
 self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activated');
   e.waitUntil(
@@ -86,6 +96,10 @@ self.addEventListener('activate', function (e) {
     })
   ); // end e.waitUntil
 });
+
+
+*/
+
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
@@ -114,6 +128,8 @@ self.addEventListener('fetch', event => {
 });
 */
 
+
+/* this is test
 this.addEventListener('fetch', event => {
   // request.mode = navigate isn't supported in all browsers
   // so include a check for Accept: text/html header.
@@ -134,6 +150,181 @@ this.addEventListener('fetch', event => {
     );
   }
 });
+
+
+*/
+
+
+
+
+
+
+
+
+//const OFFLINE_VERSION = 1;
+const CACHE_OFFLINE = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = './offline.html';
+
+
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_OFFLINE);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
+
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+
+
+
+self.addEventListener('fetch', (event) => {
+  // We only want to call event.respondWith() if this is a GET request for an HTML document.
+
+  // We only want to call event.respondWith() if this is a navigation request
+  // for an HTML page.
+  
+  //if (event.request.mode === 'navigate' && event.request.method === 'GET' &&
+  //  event.request.headers.get('accept').indexOf('text/html') !== -1) {
+   
+   if (event.request.mode === 'navigate'){
+     console.log('Handling fetch event for', event.request.url);
+    event.respondWith((async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE_OFFLINE);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+
+  // If our if() condition is false, then this fetch handler won't intercept the
+  // request. If there are any other fetch handlers registered, they will get a
+  // chance to call event.respondWith(). If no fetch handlers call
+  // event.respondWith(), the request will be handled by the browser as if there
+  // were no service worker involvement.
+});
+
+
+
+///END OFFLINE
+
+
+
+   
+   
+   
+   
+var urlsToCache = [
+  './', // Alias for index.html
+  './submit.html',
+  './termos&condicoes.html',
+  './manifest.json',
+  OFFLINE_URL
+ 
+ 
+
+
+];
+
+
+
+var CACHE_NAME = 'orcamentos-graficos';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                 console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+// We only want to call event.respondWith() if this is a GET request for an HTML document.
+// if (event.request.method === 'GET' && event.request.headers.get('accept').indexOf('text/html') !== -1) {
+   var req = event.request.clone();
+    
+    console.log('Handling fetch event for', event.request.url);
+       if (req.clone().method == "GET") {
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+          
+       }
+     
+ // }
+});
+
+self.addEventListener('fetch', function (event) {
+      var req = event.request.clone();
+    
+   
+       if (req.clone().method == "GET") {
+          
+    event.respondWith(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return fetch(event.request).then(function (response) {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        })
+    );
+       }
+});
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
