d3c33f38da58240870387576cb348b068bb4e9a9
João Gomes <joaosilgo96@gmail.com>
2021-12-19 01:12:35 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-if (navigator.serviceWorker) {
+/*if (navigator.serviceWorker) {
     navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope:',  registration.scope);
     }).catch(function(error) {
       console.log('ServiceWorker registration failed:', error);
     });
   }
+*/
